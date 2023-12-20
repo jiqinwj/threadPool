fcb80fcd22e47a0cfc2e7d10f81858f0aa5107f7
编程人生 <958940766@qq.com>
2023-12-20 17:42:39 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <functional>
+#include <thread>
+#include <future>
+#include <chrono>
+using namespace std;
+
+#include "jthreadpool.h"
+
+
+
+int sum1(int a, int b)
+{
+    this_thread::sleep_for(chrono::seconds(2));
+    
+    return a + b;
+}
+int sum2(int a, int b, int c)
+{
+    this_thread::sleep_for(chrono::seconds(2));
+    return a + b + c;
+}
+
+int main()
+{
+    ThreadPool pool;
+    // pool.setMode(PoolMode::MODE_CACHED);
+    pool.start(2);
+
+    future<int> r1 = pool.submitTask(sum1, 1, 2);
+    future<int> r2 = pool.submitTask(sum2, 1, 2, 3);
+    future<int> r3 = pool.submitTask([](int b, int e)->int {
+        int sum = 0;
+        for (int i = b; i <= e; i++)
+            sum += i;
+        return sum;
+        }, 1, 100);
+    future<int> r4 = pool.submitTask([](int b, int e)->int {
+        int sum = 0;
+        for (int i = b; i <= e; i++)
+            sum += i;
+        return sum;
+        }, 1, 100);
+    future<int> r5 = pool.submitTask([](int b, int e)->int {
+        int sum = 0;
+        for (int i = b; i <= e; i++)
+            sum += i;
+        return sum;
+        }, 1, 100);
+    //future<int> r4 = pool.submitTask(sum1, 1, 2);
+
+    cout << r1.get() << endl;
+    cout << r2.get() << endl;
+    cout << r3.get() << endl;
+    cout << r4.get() << endl;
+    cout << r5.get() << endl;
+
+   
+}
